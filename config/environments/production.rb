34fb8c4e61cff8190df03fb02fb15e0e8d24425e
mutao-net <sasaki.h1026@gmail.com>
2018-11-12 00:48:26 +0900

    delete datetime index

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.time_zone = 'Tokyo'
+  config.active_record.default_timezone = :local
 end
